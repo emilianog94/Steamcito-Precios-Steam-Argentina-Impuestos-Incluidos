b5374356ec0b4f1ac12025993eaaaf6da08d58eb
Emiliano Gioia <emilianogioia.dg@gmail.com>
2020-10-08 00:41:52 -0300

    Cambio orden de archivos

---
+++ chrome_extension/js/data.js
@@ -0,0 +1,17 @@
+const taxes = [
+    {
+        name : "IVA Servicios Digitales",
+        value : 21,
+        moreInfo: "https://www.afip.gob.ar/iva/servicios-digitales/obligados.asp"
+    },
+    {
+        name : "Impuesto PAIS",
+        value : 8,
+        moreInfo: "https://www.afip.gob.ar/impuesto-pais/caracteristicas/definicion.asp"
+    },
+    {
+        name : "Retención de Impuesto a las ganancias",
+        value : 35,
+        moreInfo: "https://www.afip.gob.ar/impuesto-pais/caracteristicas/definicion.asp"
+    }
+];
--- chrome_extension/js/global_functions.js
+++ chrome_extension/js/global_functions.js
@@ -1,21 +1,3 @@
-const taxes = [
-    {
-        name : "IVA Servicios Digitales",
-        value : 21,
-        moreInfo: "https://www.afip.gob.ar/iva/servicios-digitales/obligados.asp"
-    },
-    {
-        name : "Impuesto PAIS",
-        value : 8,
-        moreInfo: "https://www.afip.gob.ar/impuesto-pais/caracteristicas/definicion.asp"
-    },
-    {
-        name : "Retención de Impuesto a las ganancias",
-        value : 35,
-        moreInfo: "https://www.afip.gob.ar/impuesto-pais/caracteristicas/definicion.asp"
-    }
-];
-
 const attributeName = "data-original-price";
 const walletBalance = 850; // getBalance();
 const totalTaxes = getTotalTaxes();

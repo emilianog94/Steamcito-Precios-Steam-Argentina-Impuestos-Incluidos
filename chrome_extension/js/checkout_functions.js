bd29cf4fd9074a6a4d439051414c6ca7b149fea1
Emiliano Gioia <emilianogioia.dg@gmail.com>
2020-11-07 23:22:20 -0300

    cambios checkout

--- chrome_extension/js/checkout_functions.js
+++ chrome_extension/js/checkout_functions.js
@@ -20,20 +20,33 @@ function PaymentMethod(div,type){
     }
 }
 
+// Arreglar porque se rompe en el refresh cuando le ponés modify. 
+// Debería almacenar en el objeto el precio original, y siempre hacer el cálculo sobre el precio original.
 function isReviewTab(){
     let reviewTab = document.querySelector("#review_tab:not(.modified)");
-    if(reviewTab.style.display == "block"){
-        console.log("Estamos en ultimostep");
+    if(reviewTab){
+        if(reviewTab.style.display == "block"){
+            console.log("Estamos en ultimostep");
+    
+            let currentMethods = Array.from(document.querySelectorAll(".checkout_review_payment_method_area .payment_method_review_row"));
+            currentMethods = currentMethods.map(method => {
+                if(method.innerText.includes("Visa") || method.innerText.includes("MasterCard")){
+                    return new PaymentMethod(method,"cc");
+                }
+                return new PaymentMethod(method,"wallet");
+            });
+            console.log(currentMethods);
+            currentMethods.forEach(method => method.setPrice());
+            reviewTab.classList.add("modified");
 
-        let currentMethods = Array.from(document.querySelectorAll(".checkout_review_payment_method_area .payment_method_review_row"));
-        currentMethods = currentMethods.map(method => {
-            if(method.innerText.includes("Visa") || method.innerText.includes("MasterCard")){
-                return new PaymentMethod(method,"cc");
+            let returnButtons = reviewTab.querySelectorAll("a");
+            returnButtons.forEach(button => button.addEventListener('click',resetReviewTab));
+
+            function resetReviewTab(){
+                reviewTab.classList.remove("modified");
+                console.log("borré modified");
             }
-            return new PaymentMethod(method,"wallet");
-        });
-        console.log(currentMethods);
-        currentMethods.forEach(method => method.setPrice());
-        reviewTab.classList.add("modified");
-    } 
+        } 
+    }
 }
+

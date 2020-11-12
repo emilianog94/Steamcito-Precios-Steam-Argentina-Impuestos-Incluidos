dc12e23d4487a1a3affad11bfa93af4f6bd714fa
Emiliano Gioia <emilianogioia.dg@gmail.com>
2020-11-12 02:17:07 -0300

    Se agrego tutorial tras instalacion

---
+++ chrome_extension/js/show-tutorial-and-log.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(function (){
+    chrome.tabs.create({url:"https://github.com/emilianog94/Steamcito-Precios-Steam-Argentina-Impuestos-Incluidos/landing/tutorial.html"},function(){})
+})

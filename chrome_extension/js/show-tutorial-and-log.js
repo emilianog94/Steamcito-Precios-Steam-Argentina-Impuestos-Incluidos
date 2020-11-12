1284563856bd675708504207328ccc552326c1ca
Emiliano Gioia <emilianogioia.dg@gmail.com>
2020-11-12 02:21:32 -0300

    FIx archivos

--- chrome_extension/js/show-tutorial-and-log.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (){
-    chrome.tabs.create({url:"https://github.com/emilianog94/Steamcito-Precios-Steam-Argentina-Impuestos-Incluidos/landing/tutorial.html"},function(){})
-})

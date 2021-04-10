6457ec5dc35e34124108883d2910b762b49b5d0c
Emiliano Gioia <emiliano@gioia.com.ar>
2021-04-10 14:16:36 -0300

    cambio base impositiva

--- chrome_extension/js/data.js
+++ chrome_extension/js/data.js
@@ -2,19 +2,14 @@ const attributeName = "data-original-price";
 
 const standardTaxes = [
     {
-        name : "IVA Servicios Digitales - RG AFIP N° 4240/2018",
-        value : 21,
-        moreInfo: "http://biblioteca.afip.gob.ar/dcp/REAG01004240_2018_05_11"
-    },
-    {
-        name : "Impuesto PAIS - RG AFIP N° 4659/2020",
-        value : 8,
-        moreInfo: "http://biblioteca.afip.gob.ar/dcp/REAG01004659_2020_01_06"
-    },
-    {
         name : "Retención del Impuesto a las ganancias - RG AFIP Nº 4815/2020",
         value : 35,
         moreInfo: "https://www.boletinoficial.gob.ar/detalleAviso/primera/235038/20200916"
+    },
+    {
+        name : "Impuesto PAIS - RG AFIP N° 4659/2020",
+        value : 30,
+        moreInfo: "http://biblioteca.afip.gob.ar/dcp/REAG01004659_2020_01_06"
     }
 ];
 

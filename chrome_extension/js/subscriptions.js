4f5a679882f909beeb875227b72aca577653c2ef
Emiliano <emilianogioia.dg@gmail.com>
2025-05-29 23:04:34 -0300

    feat: :sparkles: Reestablezco funcionalidad de Suscripciones

--- chrome_extension/js/subscriptions.js
+++ chrome_extension/js/subscriptions.js
@@ -1,11 +1,13 @@
 let precios = document.querySelectorAll(".updateSubscriptionOptionPrice,.transactionRowAmountDue,.itemSubtext,.game_area_purchase_game_dropdown_menu_item_text,.game_area_purchase_game_dropdown_selection span");
 
-// Desactivo función temporalmente
-// precios.forEach(precio => {
+console.log("Subscription spans are", precios);
+
+precios.forEach(precio => {
     
-//     precio.innerHTML = precio.innerHTML + " &nbsp;"; // Previene errores
-//     check(precio);
-// });
+    precio.innerHTML = precio.innerHTML + " &nbsp;"; // Previene errores
+    precio.innerText = precio.innerText.replace("USD","");
+    check(precio);
+});
 let spans = document.querySelectorAll(".suscription-price");
 spans.forEach(span => {
     if(walletBalance < span.innerText){
@@ -21,19 +23,24 @@ spans.forEach(span => {
 })
 
 function check(element,start = 0){
-    if(element.innerText.indexOf("ARS$ ",start) != -1)
+    console.log("CHECKING");
+    if(element.innerText.indexOf("$",start) != -1)
     {
-        let inicioNumero = element.innerHTML.indexOf("ARS$ ",start) + 5;
+        let inicioNumero = element.innerHTML.indexOf("$",start) + 1;
+        console.log("Inicio numero",inicioNumero);
         let finNumero = element.innerHTML.indexOf(" ",inicioNumero);
+        console.log("Fin numero",finNumero);
         let numeroOriginal = element.innerHTML.substring(inicioNumero,finNumero);
-        let numeroWallet = sanitize(element.innerHTML.substring(inicioNumero,finNumero));
-        let numeroArgentino = calcularImpuestos(stringToNumber(numeroWallet,"none")/100);
+        console.log("Numero original",numeroOriginal);
+        let numeroArgentino = calculateTaxesAndExchange(numeroOriginal);
+        console.log("Numero argentino",numeroArgentino);
 
         element.innerHTML = element.innerHTML.replace(
-            numeroOriginal,`<span class="suscription-price" data-argentina-price="${numeroArgentino}" data-original-price="${numeroWallet}">${numeroWallet}</span>`
+            numeroOriginal,`<span class="suscription-price" data-argentina-price="${numeroArgentino}" data-original-price="${numeroOriginal}">${numeroOriginal}</span>`
         );
 
         check(element,finNumero); // Hago un chequeo recursivo para verificar si hay más strings de ARS$
+
     } 
 }
 
